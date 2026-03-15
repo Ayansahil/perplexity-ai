bd829a7d80a224a06871a6e30abebf0b029d37a5
Ayan Sahil <ayans8557@gmail.com>
2026-03-15 16:53:14 +0530

    initialize frontend with React, Redux, and Tailwind CSS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,19 +1,24 @@
+import cors from "cors";
 import express from "express";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.routes.js";
 import helmet from "helmet";
-import cors from "cors";
 import morgan from "morgan";
 
 const app = express();
 
 // Middleware
-app.use(helmet());
-app.use(cors());
-app.use(morgan("dev"));
 app.use(express.json());
+app.use(helmet());
+app.use(morgan("combined"));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+  methods: [ "GET", "POST", "PUT", "DELETE" ],
+}))
 
 // Health Check Route
 app.get("/health", (req, res) => {
